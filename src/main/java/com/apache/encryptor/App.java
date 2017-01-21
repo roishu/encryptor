018b90c5db54767ad758c9cb2755380635869af8
Roi <Roi@LAPTOP-K8945BCG>
2017-01-21 15:01:53 +0200

    main commit

--- src/main/java/com/apache/encryptor/App.java
+++ src/main/java/com/apache/encryptor/App.java
@@ -1,13 +1,34 @@
 package com.apache.encryptor;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	public static final String[] functions = { "Encryption", "Decryption"};
+
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("Path InputDialog Example");
+        String path = JOptionPane.showInputDialog(frame, "Path :");
+        System.out.println("Path is : " + path);
+        //String choose = JOptionPane.showInputDialog(frame, " :");
+        
+        String choose = (String) JOptionPane.showInputDialog(frame, 
+                "What do you wish to do ?",
+                "Functions:",
+                JOptionPane.QUESTION_MESSAGE, null, functions, functions[0]);
+
+            // favoritePizza will be null if the user clicks Cancel
+            System.out.printf("Function is %s.\n", choose);
+
+        
+        
     }
 }
