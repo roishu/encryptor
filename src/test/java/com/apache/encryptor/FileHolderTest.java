8b6048166c57377b3c39f82770ea8c4a95836757
Roi <Roi@LAPTOP-K8945BCG>
2017-01-25 17:06:16 +0200

    test the test :D

---
+++ src/test/java/com/apache/encryptor/FileHolderTest.java
@@ -0,0 +1,12 @@
+package com.apache.encryptor;
+
+import junit.framework.TestCase;
+
+public class FileHolderTest extends TestCase {
+	
+	public void test() {
+		// TODO Auto-generated method stub
+		assertEquals(1, 1);
+	}
+
+}
