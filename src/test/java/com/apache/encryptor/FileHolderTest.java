19c34ca2d5946e69d309618c7ddcfaf2eab127b3
Roi <Roi@LAPTOP-K8945BCG>
2017-01-27 18:58:39 +0200

    try to fix mockito

--- src/test/java/com/apache/encryptor/FileHolderTest.java
+++ src/test/java/com/apache/encryptor/FileHolderTest.java
@@ -1,13 +1,14 @@
 package com.apache.encryptor;
 
 import junit.framework.TestCase;
+import org.mockito.Mockito;
 
 
 public class FileHolderTest extends TestCase {
 	
 	public void testFileHolderImport() {
 		// TODO Auto-generated method stub
-		//FileHolder fileHolderMock = mock(FileHolder.class);
+		//FileHolder fileHolderMock = Mockito.mock(FileHolder.class);
 		assertEquals(1, 1);
 	}
 
