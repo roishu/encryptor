279268596437067fcc3357811f3d3fa79cd0f450
Roi <Roi@LAPTOP-K8945BCG>
2017-05-23 13:57:39 +0300

    menu update

--- src/main/java/com/apache/encryptor/App.java
+++ src/main/java/com/apache/encryptor/App.java
@@ -2,6 +2,8 @@ package com.apache.encryptor;
 import java.io.File;
 import java.util.ArrayList;
 
+import com.apache.gui.EncryptorMenu;
+
 /**
  * Roi Shukrun
  * T.Z : 313454464
@@ -16,29 +18,8 @@ public class App
 	
     public static void main( String[] args )  throws Exception
     {
-    	//    	Path desktopPath = Paths.get("C:\\Users\\Roi\\Desktop\\files-desktop\\result1");
     	
-    	EncryptorManager encryptor = new EncryptorManager();
-       	
-    	final File folder = new File("C:\\Users\\Roi\\Desktop\\files-desktop\\result1");
-    	ArrayList<FileHolder> filesInFolder = new ArrayList<FileHolder>();
-    	ThreadPoolEncryptor tpEncryptor = new ThreadPoolEncryptor(folder, "MultiplicativeCipher");
-    	//tpEncryptor.execute();
-    	
-		//encryptor.deleteEncryptorFiles(fileHolder);
-    
-		long startTime = System.nanoTime();
-		int i = 0;
-    	    for (final File fileEntry : folder.listFiles()) {
-    	    	if(!fileEntry.isDirectory() && fileEntry.exists()){
-    	    		filesInFolder.add(i,new FileHolder(fileEntry));
-    	    		encryptor.executeBaseAlgorithm("MultiplicativeCipher",filesInFolder.get(i));
-    	    		i++;
-    	    	}
-    	    }//for
-    	    long endTime = System.nanoTime();
-    	    long duration = (endTime - startTime) / 1000000 ;
-    		System.out.println("-----------Time 2: " + duration + "ms-----------"); 
+    	EncryptorMenu menu = new EncryptorMenu();
         
     }   
     
